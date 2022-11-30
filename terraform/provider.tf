f0900051e8213c091dc5216a5f279d10d0bea4d0
fosterm-mw <michael.foster@mavenwave.com>
2022-11-30 15:33:34 -0600

    format

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-1"
+  region = var.region
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,11 @@
 
+variable "region" {
+  type    = string
+  default = "us-west-1"
+}
+
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/24"
 }
 
