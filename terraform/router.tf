48879b7c9629293a1056c810add5a4c888bd24b9
fosterm-mw <michael.foster@mavenwave.com>
2022-12-01 11:16:21 -0600

    depends on

--- terraform/router.tf
+++ terraform/router.tf
@@ -15,6 +15,10 @@ resource "aws_route_table" "private-rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.NATGW.id
   }
+  
+  depends_on {
+    resource.aws_nat_gateway.NATGW
+  }
 }
 
 resource "aws_route_table_association" "public-rt-association" {
