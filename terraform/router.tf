499ac976bc3a9c38fcca23c5f882def1f8ef86d6
fosterm-mw <michael.foster@mavenwave.com>
2022-12-01 11:24:08 -0600

    routing issues

--- terraform/router.tf
+++ terraform/router.tf
@@ -27,7 +27,7 @@ resource "aws_route_table_association" "public-rt-association" {
 }
 
 resource "aws_route_table_association" "private-rt-association" {
-  subnet_id = aws_subnet.private-rt.id
+  subnet_id = aws_subnet.private-subnet.id
   route_table_id = aws_route_table.private-rt.id
 }
 
