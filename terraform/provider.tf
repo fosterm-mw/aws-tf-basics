d96b29b1e94a728abb97b8bb85b148e29f8d11d0
fosterm-mw <michael.foster@mavenwave.com>
2022-12-01 13:08:23 -0600

    different spot?

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,9 +19,9 @@ terraform {
 }
 
 provider "kubernetes" {
-  load_config_file = false
   host = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file = false
 }
 
 provider "aws" {
